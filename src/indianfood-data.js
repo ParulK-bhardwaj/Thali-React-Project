edd66317471e9b3e0556ade613e7534b02d1b120
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-02-23 15:11:45 -0500

    Updates

--- src/indianfood-data.js
+++
@@ -1,9 +0,0 @@
-import data from './indianfood-data.json'
-
-// This file imports './indianfood-data.json', loops over the data and adds a new id property to each object. 
-
-data.forEach((obj, i) => {
-	obj.id = i
-})
-
-export default data;
